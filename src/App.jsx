c0c62c465445bfc1102babb0d73a57a8b72bbbd3
Senyo197 <Senyo197@gmail.com>
2023-08-27 11:22:17 +0000

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AboutMe from "./Components/About_me.jsx"
+
+function App() {
+
+  return (
+    <>
+      <AboutMe />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/About_me.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutMe (){
+    return (
+        <>
+            <div className="About">
+                <h1>My Portfolio</h1>
+            </div>
+        </>
+)
+
+}
+
+export default AboutMe;
