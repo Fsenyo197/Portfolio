5b6be22d3b18df08c5b8ec14299af99e1f56f8ff
Senyo197 <Senyo197@gmail.com>
2023-08-30 19:17:19 +0000

    Changes to the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AboutMe from "./Components/About_me.jsx"
+import ContactForm from "./Components/Contact_form.jsx"
 
 function App() {
 
   return (
     <>
       <AboutMe />
+      <ContactForm />
     </>
   )
 }
---
+++ src/Components/Contact_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function ContactForm() {
+    return (
+        <>
+            <div>
+                <label htmlFor="">Contact</label>
+                <form action="">
+                    <label htmlFor="Name">Name: </label>
+                    <input type="textarea" />
+                    <label htmlFor="Email">Email: </label>
+                    <input type="textarea" />
+                    <button type = "Submit">Send</button>
+                </form>
+             </div>
+        </>
+    )
+}
+ 
