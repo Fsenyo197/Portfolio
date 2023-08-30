b57399a27b7f70322f06b123a0f023c34f00fbdf
Senyo197 <Senyo197@gmail.com>
2023-08-30 19:16:31 +0000

    Created a new component

--- src/Components/About_me.jsx
+++ src/Components/About_me.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-function AboutMe (){
+export default function AboutMe (){
     return (
         <>
             <div className="About">
                 <h1>My Portfolio</h1>
             </div>
         </>
-)
+    )
 
 }
-
-export default AboutMe;
