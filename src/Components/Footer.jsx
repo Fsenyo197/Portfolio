637608ad046ce4f358b39f6c6e8776359d47a4d0
Senyo197 <psenyo197@gmail.com>
2024-04-26 21:34:53 +0000

    Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <footer className="footer mt-32 flex justify-center items-center">
+        <footer className="footer mt-36 flex justify-center items-center">
             <div className="text-gray-600 text-sm">
                 <span className="mr-2">&copy; {new Date().getFullYear()}</span>
                 <span className="font-semibold">Richard Senyo.</span> All rights reserved.
