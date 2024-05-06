826de61c3e081bd15000b9d2bf32fc7fba047302
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-05-06 15:36:13 +0000

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,18 +1,66 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const setupMiddleware = require("./middleware");
-const setupRoutes = require("./routes");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const api = express();
 const router = express.Router();
 
-setupMiddleware(api);
+// Middleware
+api.use(bodyParser.json());
+api.use(cors({
+  origin: 'https://richard-senyo.netlify.app/', // Specify your frontend domain
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 
-setupRoutes(router);
+// Default route to check if the app is running
+router.get("/", (req, res) => {
+  res.send("MailServer is running..");
+});
+
+// Route to send an email
+router.post("/send-email", async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Basic input validation
+  if (!name || !email || !subject || !message) {
+    return res.status(400).send("All fields are required");
+  }
+
+  try {
+    // Create a nodemailer transporter
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'psenyo197@gmail.com', // Replace with your Gmail email
+        pass: 'nlly nfvu euar yzpk', // Replace with your Gmail password or app-specific password
+        //user: process.env.EMAIL_USER,
+        //pass: process.env.EMAIL_PASS,
+      }
+    });
+
+    // Send email
+    let info = await transporter.sendMail({
+      from: `"${name}" <${email}>`,
+      to: process.env.DESTINATION_EMAIL,
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+
+    res.sendStatus(200); // Send success response
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Error sending email"); // Send error response
+  }
+});
 
 // Mounting the router at the specified path
-api.use("/", router);
+api.use("/.netlify/functions/api", router);
 
+// Export handler for serverless deployment
 module.exports = api;
 module.exports.handler = serverless(api);
-
