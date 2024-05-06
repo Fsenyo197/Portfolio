8893f87f1ea0cdfe50d8af43b358d15d11bdef30
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-05-06 06:10:42 +0000

    Update middleware.js

--- functions/middleware.js
+++ functions/middleware.js
@@ -5,7 +5,7 @@ const setupMiddleware = (api) => {
   api.use(bodyParser.json());
   api.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "https://richard-senyo.netlify.app");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
