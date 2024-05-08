af18ba44f689c7f111b1d86ec87d3b87e3324a0e
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-05-08 04:07:28 +0000

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -11,7 +11,7 @@ api.use(bodyParser.json());
 
 // CORS settings
 api.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  //res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
