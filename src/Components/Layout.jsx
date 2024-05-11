0f07a2b1e7010bad1feeb5f816c2bc3abb7fc05c
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-05-11 06:00:44 +0000

    Update Layout.jsx

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -12,10 +12,10 @@ export default function Layout({ children, isNavbarVisible }) {
       </div>
       <div className="resize">
         <div className="flex flex-col">
-          <div className={`w-screen sticky top-0 ${isNavbarVisible ? 'fixed sticky top-0' : ''}`}>
+          <div className={`w-screen sticky top-0}>
             <Navbar isNavbarVisible={isNavbarVisible} />
           </div>
-          <main className="p-8 lg:p-24">{children}</main>
+          <main className={`p-8 lg:p-24 ${isNavbarVisible ? 'fixed top-0 left-0 right-0' : ''}`}>{children}</main>
         </div>
       </div>
     </div>
